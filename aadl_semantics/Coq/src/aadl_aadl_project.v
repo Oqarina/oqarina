d2da74f459f9b44d9cd936ba8b4f466d1544bebd
jjhugues <jhugues@andrew.cmu.edu>
2021-04-15 09:12:13 -0400

    * Add some rationale

--- aadl_semantics/Coq/src/aadl_aadl_project.v
+++ aadl_semantics/Coq/src/aadl_aadl_project.v
@@ -1,6 +1,9 @@
 (** %\chapter{\texttt{AADL\_Project}} %*)
 
-(** Loose mapping of aadl_project.aadl to define common types, units, etc.*)
+(** Loose mapping of aadl_project.aadl to define common types, units, etc.
+
+Some elemenets like Support_xxx enumerators are defined in the corresponding property set mappings. It does not make sense to separate them, and never ever do we want to allow them to be modified by the user
+*)
 
 Require Import time.
 
