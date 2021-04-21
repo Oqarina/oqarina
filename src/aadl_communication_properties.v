44fc0e0eebbcf9cd37c1c9ed4b05bec35093e3b2
jjhugues <jhugues@andrew.cmu.edu>
2021-04-21 17:29:41 -0400

    * Add lemmas for input_time and Queue_Processing_Protocol

--- src/aadl_communication_properties.v
+++ src/aadl_communication_properties.v
@@ -4,6 +4,7 @@
 (** Coq Library *)
 Require Import List.
 Import ListNotations. (* from List *)
+Require Import Coq.Lists.ListDec.
 
 (** AADL library *)
 Require Import aadl_aadl_project.
@@ -13,6 +14,15 @@ Require Import utils.
 (** ** %\texttt{Communication\_Properties}% as Coq native types
 
 %
+  \begin{definition}[Queue\_Processing\_Protocol (Coq)]
+ TBD. This version
+  \end{definition} %
+*)
+
+Inductive Queue_Processing_Protocol :=
+| FIFO.
+
+(** %
   \begin{definition}[IO\_Time\_Spec (Coq)]
  TBD. This version
   \end{definition} %
@@ -36,9 +46,27 @@ Proof.
   unfold eq_dec.
   decide equality.
   apply list_eq_dec; apply IO_Time_Spec_eq_dec.
-Qed.
+Defined.
 
 Definition projectionIO_Time_Spec (i : input_time) :=
   match i with
   | Input_Time t => t
 end.
+
+(** %
+  \begin{wfrule}[IO\_Time\_Spec Well-formedness rule (Coq)]
+  An input time is well-formed iff. there is no duplicate in the list of IO\_Time\_Spec.
+  \end{wfrule} %
+*)
+
+Definition input_time_wf  (i : input_time) :=
+  NoDup (projectionIO_Time_Spec i).
+
+Definition input_time_wf_dec : forall i : input_time, dec_sumbool (input_time_wf i).
+Proof.
+  unfold dec_sumbool.
+  intros.
+  unfold input_time_wf.
+  apply NoDup_dec'.
+  apply IO_Time_Spec_eq_dec.
+Defined.
