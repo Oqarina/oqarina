f82c85a669ae680f0b8f1aa26e82b353d50b72e4
jjhugues <jhugues@andrew.cmu.edu>
2021-06-08 16:34:57 -0400

    * Remove useless files

--- src/properties/extraction.v
+++
@@ -1,9 +0,0 @@
-(* extraction.v *)
-
-From Coq Require Extraction.
-Require Coq.extraction.ExtrOcamlBasic.
-Require Coq.extraction.ExtrOcamlNativeString.
-
-Require Import properties.
-
-Extraction Library properties.
