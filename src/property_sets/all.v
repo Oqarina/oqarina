5d6885cd2204143b703bfe42b559c979b2023d64
jjhugues <jhugues@andrew.cmu.edu>
2021-07-01 19:43:52 -0400

    * Minor reformatting

--- src/property_sets/all.v
+++ src/property_sets/all.v
@@ -2,4 +2,3 @@ Require Export Oqarina.property_sets.aadl_aadl_project.
 Require Export Oqarina.property_sets.aadl_communication_properties.
 Require Export Oqarina.property_sets.aadl_thread_properties.
 Require Export Oqarina.property_sets.aadl_timing_properties.
-
