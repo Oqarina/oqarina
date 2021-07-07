567453b95931a1acf1cafc869f49daea71497772
jjhugues <jhugues@andrew.cmu.edu>
2021-07-07 11:46:56 -0400

    * Update system definition to use actor

--- src/aadl_dynamic_system.v
+++ src/aadl_dynamic_system.v
@@ -1,5 +1,5 @@
 (**
-%\section{AADL System Category}\label{sec::aadl_system}%
+%\chapter{AADL System Category}\label{sec::aadl_system}%
 
 %\N% The %\texttt{system}% component category denotes an assembly of interacting application software, execution platform, and sub-systems as sub-components. Systems may be hierarchically nested%\footnote{Note: the following is inspired from AADLv2.2 \S 13.3. We heavily simplified it to the bare minimal level of information. We also corrected some of this description to better reflect the modular nature of AADL instance hierarchy and remove redundant information that belongs to the description of other component categories.}%.
 
@@ -48,12 +48,15 @@
 (* begin hide *)
 Require Import Coq.Init.Datatypes.
 Require Import Coq.ZArith.ZArith.
+Require Import Coq.Lists.List.
+Import ListNotations. (* from List *)
 
 Require Import Oqarina.formalisms.actor.
 Require Import Oqarina.core.time.
 Import NaturalTime.
 Require Import Oqarina.property_sets.all.
 Require Import Oqarina.coq_utils.utils.
+Require Import Oqarina.formalisms.lts.
 Set Implicit Arguments.
 
 Open Scope Z_scope.
@@ -61,13 +64,13 @@ Open Scope Z_scope.
 
 (** *** Coq mechanization
 
-    The following provides a Coq mechanization of the previous automata using p-Automata. *)
+    The following provides a Coq mechanization of the previous automata using the actor formalism. *)
 
-(** We use Coq unit as a system has no variable attached to is *)
+(** We use Coq [unit] as a system has no variable. *)
 
 Definition system_var : Type := unit.
 
-(** The informal definition provides a direct path to the definition of    states, actions and transitions.
+(** The informal definition provides a direct path to the definition of states, actions and transitions.
 *)
 
 Inductive system_states : Set :=
@@ -78,58 +81,9 @@ Scheme Equality for system_states.
 Inductive system_actions : Set :=
     start_system | abort_system | started_system | stop_system | stopped_system.
 
-Definition system_transitions
-    (a : system_actions) (t : Time)
-    (l1 : system_states) (v1 : system_var)
-    (l2 : system_states) (v2 : system_var) : Prop :=
-    match a, t, l1, v1, l2, v2 with
-    (* capture the transitions of the automata *)
-    | start_system, _, system_offline, _, system_starting, _ => True
-    | started_system, _, system_starting, _, system_operational, _ => True
-    | stop_system, _, system_operational, _, system_stoping, _ => True
-    | stopped_system, _, system_stoping, _, system_offline, _ => True
-    | abort_system, _, system_starting, _, system_offline, _ => True
-    | abort_system, _, system_operational, _, system_offline, _ => True
-    (* any other configuration is invalid *)
-    | _, _, _, _, _, _   => False
-    end.
+(************)
 
-(** An AADL system is an hybrid category, there is no invariant
-    attached to it. *)
-
-Definition system_invariants
-     (l : system_states) (t : Time) (v : system_var) : Prop :=
-    match l, t, v with
-    | _, _,_ => True
-    end.
-
-Record System_State_Variable : Type := mkSystemStateVariable {
-    current_state : system_states;
-    system_time : AADL_Time;
-}.
-
-Definition System_Initial_State : System_State_Variable := {|
-    current_state := system_offline;
-    system_time := 0;
-|}.
-
-Definition start_system_pre (ssv : System_State_Variable)  :=
-    system_states_beq ssv.(current_state) system_offline.
-
-Definition start_system_op (ssv : System_State_Variable) :=
-    if start_system_pre ssv then
-        {| current_state := system_starting; system_time := 0; |}
-    else
-        ssv.
-
-Example s := start_system_op  System_Initial_State .
-Compute s.
-
-(***********
-
-*)
-
-Definition system_Fire (s : system_states) (a : system_actions) : unit := tt.
+Definition system_Fire (s : system_states) (a : system_actions) : list system_actions := nil.
 
 Definition system_Post_Fire
     (s : system_states)
@@ -145,23 +99,27 @@ Definition system_Post_Fire
     (* any other configuration is invalid *)
     | _, _ => s
     end.
-(*
-Definition system_Deadline
-    (s : system_states)  (i : system_actions) : Time :=
-    0.
+
+Definition system_Deadline (s : system_states)  (i : system_actions) : Time :=
+    0%nat.
 
 Definition system_Time_Update
     (s : system_states)  (i : system_actions) (t : Time) : system_states :=
     s.
 
-Definition System_Actor : Actor system_states system_actions unit := {|
+Definition System_Actor : Actor system_states system_actions := {|
     Initial_State := system_offline;
     Fire := system_Fire;
     Post_Fire := system_Post_Fire;
     Deadline := system_Deadline;
     Time_Update := system_Time_Update;
 |}.
-*)
 
+(** Example *)
 
+(** From the previous elements, one can step a system *)
 
+Definition System_LTS := LTS_Of (System_Actor).
+
+Example f' := step_lts System_LTS (Init System_LTS) (Dis [start_system ; started_system]).
+Compute f'.
