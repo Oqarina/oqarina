7fa216582a6bda6f9f6830adde83f25eaa5ea34c
jjhugues <jhugues@andrew.cmu.edu>
2021-07-27 08:35:09 -0400

    * Add all.v

---
+++ src/AADL/Kernel/all.v
@@ -0,0 +1,6 @@
+Require Export Oqarina.AADL.Kernel.categories.
+Require Export Oqarina.AADL.Kernel.component.
+Require Export Oqarina.AADL.Kernel.component_wf.
+Require Export Oqarina.AADL.Kernel.properties.
+Require Export Oqarina.AADL.Kernel.typecheck.
+Require Export Oqarina.AADL.Kernel.properties_helper.
