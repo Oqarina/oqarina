416e34cfdf41b5d0738112caf7081510037e65e9
jjhugues <jhugues@andrew.cmu.edu>
2021-08-16 11:15:10 -0400

    * Adjust list of Coq files

--- src/AADL/Kernel/all.v
+++ src/AADL/Kernel/all.v
@@ -1,7 +1,6 @@
 Require Export Oqarina.AADL.Kernel.categories.
 Require Export Oqarina.AADL.Kernel.component.
 Require Export Oqarina.AADL.Kernel.components_helper.
-Require Export Oqarina.AADL.Kernel.component_wf.
 Require Export Oqarina.AADL.Kernel.properties.
 Require Export Oqarina.AADL.Kernel.typecheck.
 Require Export Oqarina.AADL.Kernel.properties_helper.
