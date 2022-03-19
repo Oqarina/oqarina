57ce92b9b70b61b5ad8ec5be2db5b6fc4dc1e423
jjhugues <jhugues@andrew.cmu.edu>
2022-03-18 22:49:14 -0400

    * Minor reformatting, add Actual_Processor_Binding_Name

--- src/AADL/property_sets/deployment_properties.v
+++ src/AADL/property_sets/deployment_properties.v
@@ -48,14 +48,14 @@ Require Import Oqarina.AADL.property_sets.aadl_project.
 Definition Deployment_Properties_PS :=
     PropertySet (Id "Deployment_Properties") [
 
-    (* 	Actual_Processor_Binding: inherit list of reference (processor, virtual processor, system, device, abstract)
+    (* Actual_Processor_Binding: inherit list of reference (processor, virtual processor, system, device, abstract)
 		applies to (thread, thread group, process, system, virtual processor, device);*)
 
     "Actual_Processor_Binding" :prop PT_Reference
       => None applies [CompCat thread ; CompCat threadGroup ; CompCat process ; CompCat system ;
                        CompCat virtualProcessor ; CompCat device] ;
 
-    (* 	Scheduling_Protocol: inherit list of Supported_Scheduling_Protocols
+    (* Scheduling_Protocol: inherit list of Supported_Scheduling_Protocols
 		applies to (virtual processor, processor, system);*)
 
     "Scheduling_Protocol" :prop PT_TypeRef (PSQN "AADL_Project" "Supported_Scheduling_Protocols")
@@ -68,3 +68,8 @@ Lemma Deployment_Properties_PS_Valid :
 Proof.
   trivial.
 Qed.
+
+Definition Actual_Processor_Binding_Name := PSQN "Deployment_Properties" "Actual_Processor_Binding".
+
+Definition Is_Actual_Processor_Binding (pa : property_association) :=
+  Is_Property_Name Actual_Processor_Binding_Name pa.
