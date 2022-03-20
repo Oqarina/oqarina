b0a573f14b2ba0b362e08d15501bf994d637ec2d
jjhugues <jhugues@andrew.cmu.edu>
2022-03-19 21:59:34 -0400

    * Add general tactics to prove decidabiltiy results 	  of well-formedness results

--- src/core/tactics.v
+++ src/core/tactics.v
@@ -29,8 +29,31 @@
  *
  * DM21-0762
 ***)
-Require Export Oqarina.core.identifiers.
-Require Export Oqarina.core.queue.
-Require Export Oqarina.core.maps.
-Require Export Oqarina.core.time.
-Require Export Oqarina.core.tactics.
+
+Require Export Oqarina.coq_utils.all.
+
+(*| We define general tactics to prove basic decidability results. It is
+made of two parts: a hint databse and a tactic. The database well_known_dec
+shall be populated with the non-trivial results necessary to conclude the proof.
+|*)
+
+Create HintDb well_know_wf_dec.
+
+(*| prove_dec is a general tactic for decidability results {P x} + {~ P x } |*)
+
+Ltac prove_dec :=
+    repeat match goal with
+    | [ |- forall x : ?T, _ ] => intros
+    | [ |- { _ _ /\ _ _ } + {~ (_ _ /\ _ _)} ] => apply dec_sumbool_and
+    | [ |- {?X _} + {~ ?X _} ] =>
+        try auto with well_know_wf_dec ; unfold X
+    | [ |- {All _ _} + {~ All _ _} ] => apply sumbool_All_dec
+    end.
+
+Ltac prove_dec2 :=
+    repeat match goal with
+    | [ |- forall x : ?T, _ ] => intros
+    | [ |- {?X _ _} + {~ ?X _ _} ] =>
+        try auto with well_know_wf_dec ; unfold X
+    | [ |- {All _ _} + {~ All _ _} ] => apply sumbool_All_dec
+    end.
