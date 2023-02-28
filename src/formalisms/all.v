56d6c8fec57217744c426c6b1b0770ac0c59e79d
jjhugues <jhugues@andrew.cmu.edu>
2023-02-28 08:13:36 -0500

    * Minor reformatting

--- src/formalisms/all.v
+++ src/formalisms/all.v
@@ -48,13 +48,15 @@ Oqarina builds on a couple of well-known formalisms to provide analysis or simul
 
 They rely on the following libraries for internal computations and manipulations:
 
-* :doc:`formalisms__Expressions_BoolExpr.v` is a representation of boolean expressions, translation to negative and disjunctive normal forms, and computation of minimum cutsets.
+* :doc:`formalisms__Expressions_Propositions.v` is a representation of propositions, translation to negative and disjunctive normal forms, and computation of minimum cutsets.
 
 * :doc:`formalisms__lts.v` is a basic library for representing and executing Labelled Transition Systems.
 
 * :doc:`formalisms__FaultTrees_Merle_Algebra.v` mechanizes Merle's algebra used in Dynamic Fault Trees manipulation.
 
 * :doc:`formalisms__Contracts_Contracts.v` formalizes contracts meta-theory and some of its instances.
+
+* :doc:`formalisms__Expressions_BoolExpr.v` is a WiP for boolean expressions
 
 Here is the detailed list of contents:
 
@@ -63,6 +65,7 @@ Here is the detailed list of contents:
 
    formalisms__DEVS_classic_all.v.rst
    formalisms__FaultTrees_all.v.rst
+   formalisms__Expressions_Propositions.v.rst
    formalisms__Expressions_BoolExpr.v.rst
    formalisms__lts.v.rst
    formalisms__FaultTrees_Merle_Algebra.v.rst
